8e1ab4f5d06d8ea2e33325ededa2d63f08e7e82f
snowypark <partsnowy@gmail.com>
2022-07-19 17:39:25 +0900

    산술

---
+++ p4.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+int main(int argc, char *argv[]) {
+	
+	int x;
+	x = 15;
+	int y = 100;
+	float z = 10.5;
+	
+	printf("x= %d\n", x);
+	
+	x = x*x;
+	printf("x= %d\n", x);
+	// % value
+	// d = 10진수로 출력
+	// \n = next line	
+	
+	x += 7;	
+	printf("value of x= %d, %d, %f\n", x, y, z);
+
+	
+	
+}
